68d565d6d80d66bc1f760ca9b1b1e9af98bb7cc9
sofielykke23 <sofielykke1@gmail.com>
2025-02-06 17:55:53 +0100

    implemented dynamic features

--- site/javascript/produkt.js
+++ site/javascript/produkt.js
@@ -1,23 +1,34 @@
 const productContainer = document.querySelector(".product");
-const productId = 1530;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const productId = urlParams.get("id");
+
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
 .then((response) => response.json())
 .then((data) =>{
     console.log("Fetched Data:", data);
 
 productContainer.innerHTML = `
-        <div>
+        <div class="billede_produkt">
+          <p class="${data.soldout && "sold_out2"}"></p>
         <img 
         src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp"
         alt="Beskrivelse af billede" 
         />
+     
         </div>
         <div class="product_text">
           <h2>${data.productdisplayname}</h2>
             <p><strong>Category:</strong> ${data.category}</p>
              <p><strong>Type:</strong> ${data.articletype}</p>
           <p><strong>Price: </strong>DKK ${data.price},-</p>
+             <span class="saleLabel ${data.discount && "isOnSale"}">
+        -${data.discount}%</span>
+      
+       
           <div><p class="addtocart">Add to cart</p></div>
+           
         </div>
     `;
   });
--- site/javascript/produktliste.js
+++ site/javascript/produktliste.js
@@ -1,6 +1,9 @@
 const listContainer = document.querySelector(".products-grid");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const category = urlParams.get("category");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${category}`)
 .then((response) => response.json())
 .then((data) => showList(data));
 
@@ -11,9 +14,13 @@ function showList(products){
         markup +=
         `<article class="product_list_container">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="Beskrivelse af billede" />
+          
           <h2>${product.productdisplayname}</h2>
           <p>DKK ${product.price},-</p>
-          <a href="product.html"><p class="read_more">Read more</p></a>
+          <p class="saleLabel ${product.discount && "isOnSale"}">-
+          ${product.discount}%</p>
+          <a href="product.html?id=${product.id}"><p class="read_more">Read more</p></a>
+          <p class="${product.soldout && "sold_out"}"></p>
         </article>`;
     })
     .join("");
